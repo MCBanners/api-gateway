d777a5c00d3aaf46b94d64314f3d0c9ce557be87
simpleauthority <jacob@algorithmjunkie.com>
2020-01-04 05:15:34 -0800

    initial commit

---
+++ src/main/java/com/mcbanners/apigateway/security/jwt/JwtHandler.java
@@ -0,0 +1,59 @@
+package com.mcbanners.apigateway.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtHandler {
+    private JwtParser parser;
+
+    @Value("${security.jwt.uri:/user/**}")
+    private String uri;
+    @Value("${security.jwt.header:Authorization")
+    private String header;
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+    @Value("${security.jwt.expiration:#{24 * 60 * 60}}")
+    private long expiration;
+    @Value("${security.jwt.secret:secret}")
+    private String secret;
+
+    @PostConstruct
+    protected void postConstruct() {
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.parser = Jwts.parser().setSigningKey(secretKey);
+    }
+
+    public Claims parse(String token) throws JwtException {
+        return parser.parseClaimsJws(token).getBody();
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public JwtParser getParser() {
+        return parser;
+    }
+}
