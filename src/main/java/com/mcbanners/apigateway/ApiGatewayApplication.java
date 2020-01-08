92f00a969b050453e6c4fa3789bd5293f52ad90e
simpleauthority <jacob@algorithmjunkie.com>
2020-01-08 02:29:47 -0800

    open the floodgates

--- src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-public class ApiGatewayApplication {
+@Configuration
+public class ApiGatewayApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
     }
 }
