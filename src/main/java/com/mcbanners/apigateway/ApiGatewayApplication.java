58ca95366ca8b8fd591e53ac587454f5d92035b5
simpleauthority <jacob@algorithmjunkie.com>
2021-12-20 13:35:37 -0800

    Eureka client, not discovery client?

--- src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,13 @@ package com.mcbanners.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @Configuration
 public class ApiGatewayApplication implements WebFluxConfigurer {
     public static void main(String[] args) {
