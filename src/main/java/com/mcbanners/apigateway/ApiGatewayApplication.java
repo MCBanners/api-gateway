775e69b7fef7860d2d3dc5ab5853b3f6cbc19f8b
simpleauthority <jacob@algorithmjunkie.com>
2021-12-20 15:30:53 -0800

    lets bring out the bazooka i guess

--- src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/mcbanners/apigateway/ApiGatewayApplication.java
@@ -3,20 +3,11 @@ package com.mcbanners.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@Configuration
-public class ApiGatewayApplication implements WebFluxConfigurer {
+public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
     }
 }
